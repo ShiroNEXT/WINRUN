8db291228544c1c160e3ed8833e1694ddcf3837c
Yuruko Inaba <135847138+ShiroNEXT@users.noreply.github.com>
2026-01-02 12:22:47 +0700

    Delete build directory

--- build/CMakeFiles/winrun.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for winrun.
-# This may be replaced when dependencies are built.
--- build/CMakeFiles/winrun.dir/winrun_autogen/EWIEGA46WW/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/winrun.dir/winrun_autogen/EWIEGA46WW/qrc_resources.cpp.o: \
- /home/yuruko/Documents/winrun/build/winrun_autogen/EWIEGA46WW/qrc_resources.cpp \
- /usr/include/stdc-predef.h
